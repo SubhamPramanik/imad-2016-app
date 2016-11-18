7afe652539e9cd8b9aef6d820b351e4a0966e7ce
SubhamPramanik <subhampramanik@gmail.com>
2016-11-19 02:47:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -19,8 +19,7 @@ button.onclick = function () {
 
 
 
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
+
 var submit = document.getElementById('submit_btn');
 submit.onclick = function() {
 
@@ -41,6 +40,8 @@ submit.onclick = function() {
                 }
 		}
 		}
+		var nameInput = document.getElementById('name');
+    var name = nameInput.value;
 	request.open('GET','http://subhampramanik.imad.hasura-app.io/submit-name?name=' + name, true);
 	request.send(null);
 };
