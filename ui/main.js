597ac3360dca2ba788f90a408f9aa1d41dbcf325
SubhamPramanik <subhampramanik@gmail.com>
2016-11-19 01:18:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,18 @@
-console.log('Loaded!');
-var img = document.getElementById('madi');
-img.onclick = function () {
-    img.style.marginLeft = '100px';
-}
+var button = document.getElementById('counter');
+
+button.onclick = function () {
+
+	var request = new XMLHttpRequest.DONE();
+
+	request.onreadystatechange = function () {
+		if (request.readyState === XMLHttpRequest.DONE){
+			if (request.readyState === 200) {
+				var counter = request.responseText;
+				var span = document.getElementById('count');
+				span.innerHTML = counter.toString();
+			}
+		}
+	}
+	request.open('GET','http://subhampramanik.imad.hasura-app.io/counter', true);
+	request.send(null);
+};
