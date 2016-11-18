577f0700124cc41eef18d12df260c877ec16a85b
SubhamPramanik <subhampramanik@gmail.com>
2016-11-19 01:22:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -12,7 +12,7 @@ button.onclick = function () {
 				span.innerHTML = counter.toString();
 			}
 		}
-	}
+	};
 	request.open('GET','http://subhampramanik.imad.hasura-app.io/counter', true);
 	request.send(null);
 };
